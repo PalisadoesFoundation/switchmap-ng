9844d3ce298f8df2092e720a0026edc5980f9147
Peter Harrison <peter@colovore.com>
2022-06-08 15:28:13 -0700

    Moved polling from poller to topology module

---
+++ switchmap/db/misc/disable.py
@@ -0,0 +1,14 @@
+"""Module for disabling table entries."""
+
+
+def post_poll_cleanup(idx_event):
+    """Disable all column rows not related to idx_event.
+
+    Args:
+        idx_event: idx_event
+
+    Returns:
+        None
+
+    """
+    pass
