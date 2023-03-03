be42b06bbfcbdd208901c5087158c2fd34e29f2a
Peter Harrison <peter@colovore.com>
2023-03-04 03:26:21 +0800

    All tests pass, dashboard device query

---
+++ tests/switchmap_/dashboard/pages/test_index.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python3
+"""Test the topology module."""
+
+import os
+import sys
+import unittest
+
+# Try to create a working PYTHONPATH
+EXEC_DIR = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.abspath(
+    os.path.join(
+        os.path.abspath(
+            os.path.join(
+                os.path.abspath(
+                    os.path.join(
+                        os.path.abspath(os.path.join(EXEC_DIR, os.pardir)),
+                        os.pardir,
+                    )
+                ),
+                os.pardir,
+            )
+        ),
+        os.pardir,
+    )
+)
+_EXPECTED = "{0}switchmap-ng{0}tests{0}switchmap_{0}dashboard{0}pages".format(
+    os.sep
+)
+if EXEC_DIR.endswith(_EXPECTED) is True:
+    # We need to prepend the path in case the repo has been installed
+    # elsewhere on the system using PIP. This could corrupt expected results
+    sys.path.insert(0, ROOT_DIR)
+else:
+    print(
+        """This script is not installed in the "{0}" directory. Please fix.\
+""".format(
+            _EXPECTED
+        )
+    )
+    sys.exit(2)
+
+
+# Create the necessary configuration to load the module
+from tests.testlib_ import setup
+
+CONFIG = setup.config()
+CONFIG.save()
+
+from switchmap.dashboard.pages import index as testimport
+
+
+class Test_RawCol(unittest.TestCase):
+    """Checks all functions and methods."""
+
+    #########################################################################
+    # General object setup
+    #########################################################################
+
+    @classmethod
+    def setUpClass(cls):
+        """Execute these steps before starting tests."""
+        # Load the configuration in case it's been deleted after loading the
+        # configuration above. Sometimes this happens when running
+        # `python3 -m unittest discover` where another the tearDownClass of
+        # another test module prematurely deletes the configuration required
+        # for this module
+        config = setup.config()
+        config.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Execute these steps when all tests are completed."""
+        # Cleanup the
+        CONFIG.cleanup()
+
+    def test_td_format(self):
+        """Testing function td_format."""
+        pass
+
+
+class TestHomePage(unittest.TestCase):
+    """Checks all functions and methods."""
+
+    #########################################################################
+    # General object setup
+    #########################################################################
+
+    @classmethod
+    def setUpClass(cls):
+        """Execute these steps before starting tests."""
+        # Load the configuration in case it's been deleted after loading the
+        # configuration above. Sometimes this happens when running
+        # `python3 -m unittest discover` where another the tearDownClass of
+        # another test module prematurely deletes the configuration required
+        # for this module
+        config = setup.config()
+        config.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Execute these steps when all tests are completed."""
+        # Cleanup the
+        CONFIG.cleanup()
+
+    def test___init__(self):
+        """Testing function __init__."""
+        pass
+
+    def test_html(self):
+        """Testing function html."""
+        # Initialize key variables
+        expected = """<div class="row">
+      <div class="col-lg-12">
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                  TEST
+              </div>
+              <!-- /.panel-heading -->
+              <div class="panel-body">
+                  <div class="table-responsive table-bordered">
+                      <table class="table">
+
+<tbody>
+<tr><td><a href="/switchmap/devices/95">device01.site01.example.org</a></td><td><a href="/switchmap/devices/96">device02.site01.example.org</a></td><td></td><td></td></tr>
+</tbody>
+</table>
+                  </div>
+                  <!-- /.table-responsive -->
+              </div>
+              <!-- /.panel-body -->
+          </div>
+          <!-- /.panel -->
+      </div>
+    </div><div class="row">
+      <div class="col-lg-12">
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                  SJC02
+              </div>
+              <!-- /.panel-heading -->
+              <div class="panel-body">
+                  <div class="table-responsive table-bordered">
+                      <table class="table">
+
+<tbody>
+<tr><td><a href="/switchmap/devices/97">device02.site02.prod.colovore.net</a></td><td><a href="/switchmap/devices/98">device01.site02.prod.colovore.net</a></td><td></td><td></td></tr>
+</tbody>
+</table>
+                  </div>
+                  <!-- /.table-responsive -->
+              </div>
+              <!-- /.panel-body -->
+          </div>
+          <!-- /.panel -->
+      </div>
+    </div>"""
+
+        # Get arguments
+        zones = [
+            {
+                "devices": [
+                    {
+                        "hostname": "device01.site01.example.org",
+                        "idxDevice": 95,
+                    },
+                    {
+                        "hostname": "device02.site01.example.org",
+                        "idxDevice": 96,
+                    },
+                ],
+                "name": "TEST",
+            },
+            {
+                "devices": [
+                    {
+                        "hostname": "device02.site02.prod.colovore.net",
+                        "idxDevice": 97,
+                    },
+                    {
+                        "hostname": "device01.site02.prod.colovore.net",
+                        "idxDevice": 98,
+                    },
+                ],
+                "name": "SJC02",
+            },
+        ]
+        class_obj = testimport.HomePage(zones)
+
+        # Test
+        result = class_obj.html()
+        self.assertEqual(result, expected)
+
+
+class TestZoneRow(unittest.TestCase):
+    """Checks all functions and methods."""
+
+    #########################################################################
+    # General object setup
+    #########################################################################
+
+    @classmethod
+    def setUpClass(cls):
+        """Execute these steps before starting tests."""
+        # Load the configuration in case it's been deleted after loading the
+        # configuration above. Sometimes this happens when running
+        # `python3 -m unittest discover` where another the tearDownClass of
+        # another test module prematurely deletes the configuration required
+        # for this module
+        config = setup.config()
+        config.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Execute these steps when all tests are completed."""
+        # Cleanup the
+        CONFIG.cleanup()
+
+    def test___init__(self):
+        """Testing function __init__."""
+        pass
+
+
+class TestDeviceTable(unittest.TestCase):
+    """Checks all functions and methods."""
+
+    #########################################################################
+    # General object setup
+    #########################################################################
+
+    @classmethod
+    def setUpClass(cls):
+        """Execute these steps before starting tests."""
+        # Load the configuration in case it's been deleted after loading the
+        # configuration above. Sometimes this happens when running
+        # `python3 -m unittest discover` where another the tearDownClass of
+        # another test module prematurely deletes the configuration required
+        # for this module
+        config = setup.config()
+        config.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Execute these steps when all tests are completed."""
+        # Cleanup the
+        CONFIG.cleanup()
+
+    def test___init__(self):
+        """Testing function __init__."""
+        pass
+
+
+class TestDevicesRow(unittest.TestCase):
+    """Checks all functions and methods."""
+
+    #########################################################################
+    # General object setup
+    #########################################################################
+
+    @classmethod
+    def setUpClass(cls):
+        """Execute these steps before starting tests."""
+        # Load the configuration in case it's been deleted after loading the
+        # configuration above. Sometimes this happens when running
+        # `python3 -m unittest discover` where another the tearDownClass of
+        # another test module prematurely deletes the configuration required
+        # for this module
+        config = setup.config()
+        config.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Execute these steps when all tests are completed."""
+        # Cleanup the
+        CONFIG.cleanup()
+
+    def test___init__(self):
+        """Testing function __init__."""
+        pass
+
+
+class TestFunctions(unittest.TestCase):
+    """Checks all functions and methods."""
+
+    #########################################################################
+    # General object setup
+    #########################################################################
+
+    @classmethod
+    def setUpClass(cls):
+        """Execute these steps before starting tests."""
+        # Load the configuration in case it's been deleted after loading the
+        # configuration above. Sometimes this happens when running
+        # `python3 -m unittest discover` where another the tearDownClass of
+        # another test module prematurely deletes the configuration required
+        # for this module
+        config = setup.config()
+        config.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Execute these steps when all tests are completed."""
+        # Cleanup the
+        CONFIG.cleanup()
+
+    def test___init__(self):
+        """Testing function __init__."""
+        pass
+
+    def test_rows(self):
+        """Testing function rows."""
+        pass
+
+
+if __name__ == "__main__":
+    # Do the unit test
+    unittest.main()
