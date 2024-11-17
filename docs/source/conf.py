fa2c46772b55df03e724c2553585ce99c5c0496b
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2024-11-17 14:04:43 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,7 +84,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = "book"
+html_theme = "sphinx_book_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
