6534b118404c064b0e33ec30b9ffc41217e08bdf
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-01-02 16:36:43 -0800

    Deleted all files

--- INSTALLATION.md
+++
@@ -1,4 +0,0 @@
-# SwitchMap-NG Installation
-
-[Please read the online Installation Guide](https://switchmap-ng.readthedocs.io/en/latest/)
-
--- switchmap/server/__init__.py
+++
@@ -1,8 +0,0 @@
-from collections import namedtuple
-
-# Important tuples
-ZoneData = namedtuple("ZoneData", "idx_zone data")
-ZoneObjects = namedtuple("ZoneObjects", "ips macs pairmacips")
-ZoneDevice = namedtuple("ZoneDevice", "idx_zone, data, filepath, config")
-EventObjects = namedtuple("EventObjects", "zones event")
-PairMacIp = namedtuple("PairMacIp", "mac ip ip_version idx_zone")
