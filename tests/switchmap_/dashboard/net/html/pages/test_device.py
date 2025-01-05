481966dc84bc01c7a78bb8c227b4b98c7a3b9cec
im-vedant <194vedantgutpa@gmail.com>
2025-01-06 03:25:00 +0530

    fixed failing test case

--- tests/switchmap_/dashboard/net/html/pages/test_device.py
+++ tests/switchmap_/dashboard/net/html/pages/test_device.py
@@ -181,7 +181,7 @@ class TestDevice(unittest.TestCase):
 <tr><td>System Description</td><td>Cisco IOS Software, C3750E Software (C3750E-UNIVERSALK9-M), Version<br>15.0(2)SE11, RELEASE SOFTWARE (fc3) Technical Support:<br>http://www.cisco.com/techsupport Copyright (c) 1986-2017 by Cisco<br>Systems, Inc. Compiled Sat 19-Aug-17 08:39 by prod_rel_team</td></tr>
 <tr><td>System sysObjectID</td><td>.1.3.6.1.4.1.9.1.516</td></tr>
 <tr><td>System Uptime</td><td>94 Days, 18:39:06</td></tr>
-<tr><td>Time Last Polled</td><td>2023-02-23 15:13:37</td></tr>
+<tr><td>Time Last Polled</td><td>2023-02-23 23:13:37</td></tr>
 </tbody>
 </table>"""
         result = self.device.system()
