58cab0f978ad4721b1c8bf77531403186bbe90ab
Mayank Jha <mayankjha0330@gmail.com>
2025-01-15 00:10:31 +0530

    added ROOT_path

--- tests/switchmap_/dashboard/table/test_event.py
+++ tests/switchmap_/dashboard/table/test_event.py
@@ -4,10 +4,46 @@ This module contains tests for the `table`, `EventTable`, and `EventsRow`
 classes from the `switchmap.dashboard.table.events` module.
 """
 
+import os
+import sys
 import unittest
 from switchmap.dashboard.table.events import table, EventTable
 from switchmap.dashboard import EventMeta
 from switchmap import SITE_PREFIX
+
+EXEC_DIR = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.abspath(
+    os.path.join(
+        os.path.abspath(
+            os.path.join(
+                os.path.abspath(
+                    os.path.join(
+                        os.path.abspath(os.path.join(EXEC_DIR, os.pardir)),
+                        os.pardir,
+                    )
+                ),
+                os.pardir,
+            )
+        ),
+        os.pardir,
+    )
+)
+_EXPECTED = (
+    "{0}switchmap-ng{0}tests{0}switchmap_{0}"
+    "dashboard{0}table".format(os.sep)
+)
+if EXEC_DIR.endswith(_EXPECTED) is True:
+    # We need to prepend the path in case the repo has been installed
+    # elsewhere on the system using PIP. This could corrupt expected results
+    sys.path.insert(0, ROOT_DIR)
+else:
+    print(
+        """This script is not installed in the "{0}" directory. Please fix.\
+""".format(
+            _EXPECTED
+        )
+    )
+    sys.exit(2)
 
 
 class TestEventTable(unittest.TestCase):
