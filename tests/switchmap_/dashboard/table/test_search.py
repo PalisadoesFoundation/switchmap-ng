07c69ad8bd0d66a2d32512d7690d2b17066b0307
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2025-01-14 21:32:15 +0530

    upated code

--- tests/switchmap_/dashboard/table/test_search.py
+++ tests/switchmap_/dashboard/table/test_search.py
@@ -9,8 +9,11 @@ from unittest.mock import patch
 
 # Add the project root directory to sys.path
 ROOT_DIR = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "../../../../")
+    os.path.join(
+        os.path.dirname(__file__), os.pardir, os.pardir, os.pardir, os.pardir
+    )
 )
+
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
