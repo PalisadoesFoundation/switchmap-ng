b9c596d9b42b7bd94c353824b13824f8de06cf6c
404Nikhil <nicksd111@gmail.com>
2025-01-30 03:22:18 +0530

    fix

--- tests/switchmap_/server/db/misc/test_rows.py
+++ tests/switchmap_/server/db/misc/test_rows.py
@@ -53,6 +53,17 @@ CONFIG = setup.config()
 CONFIG.save()
 
 from switchmap.server.db import models
+from switchmap.server.db.table import (
+    event,
+    zone,
+    device,
+    l1interface,
+    mac,
+    ip as ip_table,
+    macip,
+    oui,
+    root as root_table,
+)
 from switchmap.server.db.misc import rows
 from switchmap.server.db.table import zone
 from switchmap.server.db.table import event
