1b5f00b0228ad6385e43753dd9b5a61d5c9a25e9
HUmnanANirudh <tanirudhganesh@gmail.com>
2025-02-15 02:54:00 +0530

    Changes suggested by coderrabit

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ mkdir -p /run/secrets
 echo "${MYSQL_ROOT_PASSWORD_VALUE}" > /run/secrets/mysql_root_password
 echo "${MYSQL_PASSWORD_VALUE}" > /run/secrets/mysql_password
 
-unset MYSQL_ROOT_PASSWORD MYSQL_PASSWORD
+unset MYSQL_ROOT_PASSWORD_VALUE MYSQL_PASSWORD_VALUE
 
 exec /entrypoint.sh "$@"
