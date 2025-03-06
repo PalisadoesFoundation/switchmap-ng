1eeacf717e4b24a43831e58c7a31669481a6b6ab
Abhi-MS <abhiramims333@gmail.com>
2025-03-05 21:43:01 -0500

    Run black formatter

--- scripts/setup_hooks.py
+++ scripts/setup_hooks.py
@@ -5,6 +5,7 @@ HOOKS_DIR = ".git/hooks"
 SOURCE_HOOKS_DIR = "hooks"
 HOOK_FILE = "pre-commit"
 
+
 def install_git_hooks():
     """Installs Git hooks by copying the pre-commit hook to the .git/hooks directory and making it executable.
 
@@ -22,14 +23,17 @@ def install_git_hooks():
     destination_hook_path = os.path.join(HOOKS_DIR, HOOK_FILE)
 
     if not os.path.exists(source_hook_path):
-        raise FileNotFoundError(f"Source hook file '{source_hook_path}' not found.")
-    
+        raise FileNotFoundError(
+            f"Source hook file '{source_hook_path}' not found."
+        )
+
     shutil.copy(source_hook_path, destination_hook_path)
 
     # Make the hook file executable
     os.chmod(destination_hook_path, 0o755)
 
     print("Git hooks installed successfully.")
+
 
 # Run the function to install the hooks
 if __name__ == "__main__":
