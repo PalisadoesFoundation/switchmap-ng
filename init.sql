56c62d0b4ff8cec5702241cde3d75ef6d82c2ae6
HUmnanANirudh <tanirudhganesh@gmail.com>
2025-03-12 23:30:17 +0530

    Changes suggested by coderabbitai

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS switchmap CHARACTER SET utf8mb4 COLLATE utf8mb4_un
 
 USE switchmap;
 
-CREATE USER IF NOT EXISTS 'switchmap'@'%' IDENTIFIED WITH mysql_native_password BY 'CHANGE_ME_NOW';
+CREATE USER IF NOT EXISTS 'switchmap'@'%' IDENTIFIED WITH mysql_native_password BY '${DB_PASSWORD}';
 
 GRANT ALL PRIVILEGES ON switchmap.* TO 'switchmap'@'%';
 
