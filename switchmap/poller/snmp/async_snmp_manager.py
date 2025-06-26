71c0b3e9b557907e63dea142d3081400114d5ee9
Abhishek Raj <abhishek4code@gmail.com>
2025-06-26 15:39:36 +0530

    feat: methods for snmp_connection check added

--- switchmap/poller/snmp/async_snmp_manager.py
+++ switchmap/poller/snmp/async_snmp_manager.py
@@ -75,6 +75,8 @@ class Validate:
                     authorization=authorization
                 )
             )
+            # Try successive groups
+
 
 
 class Interact:
@@ -93,6 +95,94 @@ class Interact:
         self._poll = _poll
 
         # Fail if there is no authentication
+        if bool(self._poll.authorization) is False:
+            log_message = (
+                "SNMP parameters provided are either blank or missing." "Non existent host?"
+            )
+            log.log2die(1045, log_message)
+        
+    async def contactable(self): 
+        """Check if device is reachable via SNMP.
+        
+        Args: 
+            None
+        
+        Returns:
+           bool: True if device responds to SNMP queries, False otherwise
+        
+        """
+        #key variables
+        contactable = False
+        result = None 
+
+        #Try to reach device
+        try:
+            # Test if we can poll the SNMP sysObjectID
+            # if true, then the device is contactable
+            result = await self.sysobjectID(check_reachability=True)
+            if bool(result) is True:
+                contactable = True
+        
+        except Exception:
+            # Not Contactable
+            contactable = False
+        
+        except:
+            # Log the error message
+            log_message = "Unexpected SNMP error for device {}" "".format(
+                self._poll.hostname
+            )
+            log.log2die(1008,log_message)
+        
+        return contactable
+    
+    async def sysobjectID(self, check_reachability=False):
+        """Get the sysObjectID of the device.
+
+        Args:
+            check_reachability: Boolean indicating whether to test connectivity.
+                Some session errors are ignored to return null result.
+
+        Returns:
+            str: sysObjectID value as string, or None if not available
+        """
+        # Initialize key variables
+        oid = ".1.3.6.1.2.1.1.2.0"
+        object_id = None 
+        
+        #Get sysObjectID
+        results = await self.get(oid, check_reachability)
+        # Pysnmp already returns out value as value unlike easysnmp
+        if bool(results) is True:
+            object_id = results[oid]
+        
+        return object_id
+    
+    async def oid_exists(self, oid_to_get, context_name=""):
+        """Determine if an OID exists on the device.
+
+        Args:
+            oid_to_get: String containing OID to check
+            context_name: String containing SNMPv3 context name.
+                Default is empty string.
+
+        Returns:
+            bool: True if OID exists, False otherwise
+        """
+        # Initialize key 
+        validity = False
+
+        # Validate OID
+        
+
+       
+
+       
+    
+
+    
+    
+    
 
 
     
