4a2e26fcf2a62627b7189c647fa4354b32acd735
Abhi-MS <abhiramims333@gmail.com>
2025-07-11 12:42:05 -0400

    Migrate styles and structure from archived docs repo

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,45 +1,51 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HeroSection from "@site/src/components/HeroImage/HeroSection";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
+        <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs"
+        <div className="buttons_AeoN">
+          <a
+            className={`${styles.button} button button--primary button--lg`}
+            href="/docs/installation"
+            aria-label="Installation"
           >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+            Installation
+          </a>
+          <a
+            className={`${styles.button} button button--secondary button--lg`}
+            href="/docs/community"
+            aria-label="Join the community"
+          >
+            Community
+          </a>
         </div>
       </div>
     </header>
   );
 }
 
-export default function Home(): JSX.Element {
+export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={siteConfig.title}
       description="Description will go into a meta tag in <head />"
     >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <div className={styles["home-wrapper"]}>
+        <HomepageHeader />
+        <main>
+          <HeroSection />
+        </main>
+      </div>
     </Layout>
   );
 }
