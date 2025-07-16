e0900ef6131f097f153b12ba7cfd7fb8df0863ff
Abhi-MS <abhiramims333@gmail.com>
2025-07-16 16:03:05 -0400

    Make Docs home page responsive

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -42,9 +42,7 @@ export default function Home() {
     >
       <div className={styles["home-wrapper"]}>
         <HomepageHeader />
-        <main>
-          <HeroSection />
-        </main>
+        <HeroSection />
       </div>
     </Layout>
   );
