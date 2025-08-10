810260e3505ef96c85a7d32ec0c5a535db2d9829
Abhishek Raj <abhishek4code@gmail.com>
2025-08-10 17:49:39 +0530

    lint fixed

--- debug_async_flow.py
+++ debug_async_flow.py
@@ -7,25 +7,27 @@ import os
 import asyncio
 
 
-sys.path.insert(0,'/Users/imexyyyyy/files/gsoc/switchmap-ng');
+sys.path.insert(0, "/Users/imexyyyyy/files/gsoc/switchmap-ng")
 
 # once i make polling async then will update this according to that to check if devices are being polled asynchronously
-#for now for testing using cli_device to poll single device for debugging
+# for now for testing using cli_device to poll single device for debugging
 
 from switchmap.poller import async_poll
+
 
 def main():
     print("Switchmap polling flow debugger")
 
     try:
 
-        hostname= "162.249.37.218"
+        hostname = "162.249.37.218"
         print(f"starting debug poll for hostname: {hostname}")
 
         async_poll.run_cli_device(hostname=hostname)
-    
+
     except Exception as e:
         print(f"Error duing polling: {e}")
+
 
 if __name__ == "__main__":
     main()
